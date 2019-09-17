650c0e3915e9c2f77bc00dc4368420a5f4e2015f
Dmitriy Shelestovskiy <dmitriy.shelestovski@neurohive.io>
2019-09-17 09:55:21 +0300

    catmullspline

--- bspline/src/main/java/DrawingPlane.java
+++ bspline/src/main/java/DrawingPlane.java
@@ -8,19 +8,36 @@ public class DrawingPlane extends Component {
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         Vector2[] items = prep_points();
+//        draw_bspline(g2d, items);
+//        draw_bezier(g2d, items);
+        draw_cutmull(g2d, items);
         draw_points(g2d, items);
-        draw_bspline(g2d, items);
-        draw_bezier(g2d, items);
 
     }
     private void draw_points(Graphics2D g2d, Vector2[] points){
         g2d.setColor(Color.BLUE);
-        g2d.setStroke(new BasicStroke(2));
+        g2d.setStroke(new BasicStroke(5));
         for (Vector2 point : points) {
             int x = (int) Math.ceil(point.x);
             int y = (int) Math.ceil(point.y);
             g2d.drawLine(x, y, x, y);
         }
+    }
+
+    private void draw_cutmull(Graphics2D g2d, Vector2[] points) {
+        g2d.setStroke(new BasicStroke(2));
+        g2d.setColor(Color.MAGENTA);
+        Vector2 out_vect = new Vector2();
+        Vector2 tmp_vect = new Vector2();
+
+        for (int i = 0; i < 1000; i++){
+            float cur_pos = (float) (i*0.001);
+            CatmullRomSpline.calculate(out_vect, cur_pos, points, false, tmp_vect);
+            int x = (int) out_vect.x;
+            int y = (int) out_vect.y;
+            g2d.drawLine(x, y, x, y);
+        }
+
     }
 
     private void draw_bezier(Graphics2D g2d, Vector2[] points) {
